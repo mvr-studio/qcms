58bee7bc20127f82154a87c7dd9e0708adf05d3f
Tomek Marciniak <tmarciniakm@gmail.com>
2022-03-11 13:18:15 +0100

    fix(server): fix gitignore

---
+++ apps/server/examples/simple-blog/qcms.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  name: 'Simple Blog',
+  schema: {
+    post: {
+      fields: [
+        { name: 'name', type: 'String', required: true },
+        { name: 'content', type: 'String' },
+        {
+          name: 'categories',
+          type: 'Relation',
+          relation: 'hasMany',
+          model: 'Category'
+        }
+      ],
+      permissions: {
+        findOne: true,
+        findAll: true,
+        create: ({ user }) => user.role === 'ADMIN',
+        update: ({ user }) => user.role === 'ADMIN',
+        delete: ({ user }) => user.role === 'ADMIN'
+      }
+    },
+    category: {
+      fields: [
+        { name: 'name', type: 'String', required: true },
+        {
+          name: 'posts',
+          type: 'Relation',
+          relation: 'hasMany',
+          model: 'Post'
+        }
+      ],
+      permissions: {
+        findOne: true,
+        findAll: true,
+        create: ({ user }) => user.role === 'ADMIN',
+        update: ({ user }) => user.role === 'ADMIN',
+        delete: ({ user }) => user.role === 'ADMIN'
+      }
+    }
+  }
+}
